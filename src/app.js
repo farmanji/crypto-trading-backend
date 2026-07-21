b82065a09e79a59e2dd2fc0e012391f32c7bc110
Muhammad Farman <farman4k@gmail.com>
2026-07-21 21:48:24 +0300

    small fixed

--- src/app.js
+++ src/app.js
@@ -5,16 +5,15 @@ import userRouter from './controllers/userRoute.js';
 
 export const app = express();
 
-// 1. Sabhi origins ko temporary allow karein testing ke liye
+
 app.use(cors({
-  origin: true, // Ye dynamically request bhejnewale origin ko allow kar dega
+  origin: true, 
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
-// 2. Preflight (OPTIONS) requests ke liye handle karein
-app.options('*', cors()); 
+app.options('(.*)', cors());
 
 app.use(express.json());
 
